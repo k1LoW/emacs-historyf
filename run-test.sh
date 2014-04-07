968f0615837a04bfbbecfe59822bed365a01e7f0
k1LoW <k1lowxb@gmail.com>
2014-04-08 08:18:07 +0900

    Update test envrionment

--- run-test.sh
+++ run-test.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
-EMACS=emacs
-OUTPUT=/tmp/.el-expectations
-$EMACS -q --no-site-file --batch  -L . -L t/extlib/  -l el-expectations -l historyf -f batch-expectations $OUTPUT ./historyf.el
+if [ -z "$EMACS" ] ; then
+    EMACS=emacs
+fi
+OUTPUT=/tmp/.ert-expectations
+cask exec $EMACS -q --no-site-file --batch  -L . -l el-expectations -l historyf -f batch-expectations $OUTPUT ./historyf.el
 ret=$?
 cat $OUTPUT
 rm $OUTPUT
